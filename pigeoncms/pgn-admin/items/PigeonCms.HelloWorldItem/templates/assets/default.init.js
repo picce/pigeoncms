ae1266cf69444b3cde2e603fb865696c13cdba74
picce <picce@yahoo.it>
2017-05-18 18:12:09 +0200

    init working on js dynamic resources for template

--- pigeoncms/pgn-admin/items/PigeonCms.HelloWorldItem/templates/assets/default.init.js
+++ pigeoncms/pgn-admin/items/PigeonCms.HelloWorldItem/templates/assets/default.init.js
@@ -1,2 +1,20 @@
-﻿//js resource for default item template
-var pgn_helloworldItem_sample = 3;
+﻿Type.registerNamespace("PigeonCms.ItemTemplates.PigeonCms_HelloWorld_Default");
+
+
+//js resource for default item template
+PigeonCms.ItemTemplates.PigeonCms_HelloWorld_Default = function () {
+
+    this.init = function () {
+
+        var pgn_helloworldItem_sample = 3;
+
+    };
+
+}
+
+$(document).ready(function () {
+
+    template = new PigeonCms.ItemTemplates.PigeonCms_HelloWorld_Default();
+    template.init();
+
+});
