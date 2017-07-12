5e879683080fd881f1c4c714d9391920143f32af
picce <picce@yahoo.it>
2017-07-12 18:14:32 +0200

    added Redis static Lazy store with xml config added test: CheckExpiration, RedisHashCheck

---
+++ projects/PigeonCms.Core/Helpers/RedisStore.cs
@@ -0,0 +1,46 @@
+﻿/***************************************************
+PigeonCms - Open source Content Management System 
+https://github.com/picce/pigeoncms
+Copyright © 2017 Nicola Ridolfi - picce@yahoo.it
+Licensed under the terms of "GNU General Public License v3"
+For the full license text see license.txt or
+visit "http://www.gnu.org/licenses/gpl.html"
+***************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Caching;
+using System.Diagnostics;
+using System.Collections;
+using StackExchange.Redis;
+using System.Configuration;
+
+namespace PigeonCms.Core.Helpers
+{
+    public class RedisStore
+    {
+        private static readonly Lazy<ConnectionMultiplexer> lazyConnection;
+
+        static RedisStore()
+        {
+            string redisConnection = ConfigurationManager.AppSettings["RedisConnection"].ToString();
+
+            //var configurationOptions = new ConfigurationOptions
+            //{
+            //    EndPoints = { redisConnection }
+            //};
+
+            //lazyConnection = new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect(configurationOptions));
+            lazyConnection = new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect(redisConnection));
+        }
+
+        public static ConnectionMultiplexer Connection => lazyConnection.Value;
+
+        public static IDatabase RedisCache => Connection.GetDatabase();
+
+    }
+
+}
---
+++ projects/PigeonCms.Test/Helpers/RedisTest.cs
@@ -0,0 +1,166 @@
+﻿using PigeonCms.Geo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using StackExchange.Redis;
+using System.Threading.Tasks;
+using PigeonCms.Core.Helpers;
+
+namespace PigeonCms.UT.Core
+{
+
+
+    /// <summary>
+    ///dapper integration test
+    ///https://github.com/MicrosoftArchive/redis/releases (win server)
+    ///https://stackexchange.github.io/StackExchange.Redis/ (c# client)
+    
+    ///</summary>
+    [TestClass()]
+    public class RedisTest
+    {
+
+        private TestContext testContextInstance;
+        private IDatabase redis;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        [ClassInitialize()]
+        public static void MyClassInitialize(TestContext testContext)
+        {
+
+        }
+
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        
+
+        //Use TestInitialize to run code before running each test
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            redis = RedisStore.RedisCache;
+        }
+
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+        const string TEST_KEY = "pigeoncms.test.key1";
+        const string TEST_VALUE = "hello this is a test value";
+
+
+        /// <summary>
+        ///test for StringSet and StringGet methods
+        ///</summary>
+        [TestMethod()]
+        public void WriteRead()
+        {
+            redis.StringSet(TEST_KEY, TEST_VALUE);
+            string actual = redis.StringGet(TEST_KEY);
+
+            Assert.AreEqual(TEST_VALUE, actual);
+        }
+
+        [TestMethod()]
+        public void CheckDelete()
+        {
+            redis.StringSet(TEST_KEY, TEST_VALUE);
+            redis.KeyDelete(TEST_KEY);
+            Assert.IsFalse(redis.KeyExists(TEST_KEY));
+
+            string actual = redis.StringGet(TEST_KEY);
+            Assert.IsNull(actual);
+        }
+
+        [TestMethod()]
+        public void CheckExpiration()
+        {
+            redis.StringSet(TEST_KEY, TEST_VALUE, new TimeSpan(0, 0, 1), When.Always);
+            Assert.IsTrue(redis.KeyExists(TEST_KEY));
+
+            Task.Delay(1100).Wait();
+            Assert.IsFalse(redis.KeyExists(TEST_KEY), "key is not expired successfully");
+
+        }
+
+
+        [TestMethod()]
+        public void RedisHashCheck()
+        {
+            var hashKey = "hashKey";
+
+            HashEntry[] redisBookHash = {
+                new HashEntry("title", "My awesome redis"),
+                new HashEntry("year", 1980),
+                new HashEntry("author", "Pigeonne")
+            };
+
+            redis.HashSet(hashKey, redisBookHash);
+
+            if (redis.HashExists(hashKey, "year"))
+            {
+                var year = redis.HashGet(hashKey, "year");
+                Assert.IsTrue(year == 1980);
+            }
+
+            var allHash = redis.HashGetAll(hashKey);
+
+            //get all the items
+            foreach (var item in allHash)
+            {
+                //output 
+                //key: title, value: Redis for .NET Developers
+                //key: year, value: 2016
+                //key: author, value: Taswar Bhatti
+                if (item.Name == "title")
+                    Assert.IsTrue("My awesome redis" == item.Value);
+                else if (item.Name == "year")
+                    Assert.IsTrue(1980 == item.Value);
+                else if (item.Name == "author")
+                    Assert.IsTrue("Pigeonne" == item.Value);
+            }
+
+
+            var len = redis.HashLength(hashKey);  //result of len is 3
+            Assert.IsTrue(len == 3);
+
+            if (redis.HashExists(hashKey, "year"))
+            {
+                var year = redis.HashIncrement(hashKey, "year", 1);
+                var year2 = redis.HashDecrement(hashKey, "year", 1);
+
+                Assert.IsTrue(year == 1981);
+                Assert.IsTrue(year2 == 1980);
+            }
+
+        }
+    }
+
+}
